d27dcfa1860b5240527b24eb1f2c3161aad69376
yanzhang25 <yan.zhang@inf.ethz.ch>
2022-11-16 13:02:13 +0100

    add supports of gcc 9.4.0

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -6,11 +6,11 @@ PYTHON_EXECUTABLE=/home/yzhang/miniconda3/envs/particlesfm/bin/python
 
 
 # build point trajectory optimizer
-# cd point_trajectory/optimize
-# mkdir -p build && cd build
-# cmake -DPYTHON_EXECUTABLE=${PYTHON_EXECUTABLE} ..
-# make -j
-# cd ../../../
+cd point_trajectory/optimize
+mkdir -p build && cd build
+cmake -DPYTHON_EXECUTABLE=${PYTHON_EXECUTABLE} ..
+make -j
+cd ../../../
 
 # build global mapper
 cd sfm/gmapper
