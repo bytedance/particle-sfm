7eaa92887a517c0454a8c6b92e80277677b78936
priyadarsi mishra <santoshpriyadarsi@gmail.com>
2023-02-28 01:42:14 +0000

    fixing the path for python executable for env

--- scripts/build_all.sh
+++ scripts/build_all.sh
@@ -2,7 +2,7 @@
 
 # set your customized python executable
 # PYTHON_EXECUTABLE=/media/shaoliu/anaconda/envs/particlesfm/bin/python
-PYTHON_EXECUTABLE=/data/anaconda3/envs/particlesfm/bin/python
+PYTHON_EXECUTABLE=/data/anaconda3/envs/microtel/bin/python
 
 
 # build point trajectory optimizer
