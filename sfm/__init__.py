91a9124c5dd2c8a6871862db83ca3eb343c7545a
guohengkai <guohengkai@bytedance.com>
2025-02-05 19:37:07 +0800

    support glomap

--- sfm/__init__.py
+++ sfm/__init__.py
@@ -1,2 +1,2 @@
-from .main_sfm import main_global_sfm, main_incremental_sfm
+from .main_sfm import main_global_sfm, main_incremental_sfm, main_global_sfm_glomap
 from .convert import write_depth_pose_from_colmap_format
